d332c3b2661d0ef8f6a481ffff744b8002951c94
RuFrontEnd <k718198@gmail.com>
2022-06-03 15:17:16 +0800

    新增全域 css

--- net-ninja-tutorial/components/Footer.js
+++ net-ninja-tutorial/components/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div>CopyRight 2021 Ninja List</div>;
+  return <footer>CopyRight 2021 Ninja List</footer>;
 };
 
 export default Footer;
--- net-ninja-tutorial/pages/index.js
+++ net-ninja-tutorial/pages/index.js
@@ -9,7 +9,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quisquam
@@ -24,7 +23,6 @@ export default function Home() {
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
