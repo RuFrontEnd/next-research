a6d6d8dd40b6538e57d737f5100a4583c89987bb
RuFrontEnd <k718198@gmail.com>
2022-06-03 18:58:00 +0800

    新增 logo

--- net-ninja-tutorial/components/Navbar.js
+++ net-ninja-tutorial/components/Navbar.js
@@ -1,11 +1,13 @@
 // 開啟 JSX emmet => https://learningsky.io/vs-code-to-enable-emmet-support-for-jsx/
 import Link from "next/link";
+import logo from "@/public/logo.png";
+import Image from "next/image"; // lazy loading
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src={logo.src} width={128} height={77} />
       </div>
       <Link href="/">
         <a href="">Home</a>
