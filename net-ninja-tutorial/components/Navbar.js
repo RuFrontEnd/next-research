915fa865956dd6bef95e7b826c10ee2d83d1d9ca
RuFrontEnd <k718198@gmail.com>
2022-06-03 13:15:42 +0800

    新增 Next Link

--- net-ninja-tutorial/components/Navbar.js
+++ net-ninja-tutorial/components/Navbar.js
@@ -1,13 +1,21 @@
 // 開啟 JSX emmet => https://learningsky.io/vs-code-to-enable-emmet-support-for-jsx/
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
         <h1>Ninja List</h1>
       </div>
-      <a href="">Home</a>
-      <a href="">About</a>
-      <a href="">Ninja Listing</a>
+      <Link href="/">
+        <a href="">Home</a>
+      </Link>
+      <Link href="/about">
+        <a href="">About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a href="">Ninja Listing</a>
+      </Link>
     </nav>
   );
 };
--- net-ninja-tutorial/pages/index.js
+++ net-ninja-tutorial/pages/index.js
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -19,6 +21,10 @@ export default function Home() {
         quae nisi? Fuga eaque officia tenetur fugit assumenda et nihil animi
         laboriosam quas autem, veniam, quo consequatur unde facere consequuntur.
       </p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
+      <Footer />
     </div>
   );
 }
