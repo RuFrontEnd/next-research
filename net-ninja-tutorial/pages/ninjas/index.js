3c996f819ddba6168adce2a982b2280d27fb00ee
RuFrontEnd <k718198@gmail.com>
2022-06-03 19:14:25 +0800

    新增 getStaticProps 方法

--- net-ninja-tutorial/pages/ninjas/index.js
+++ net-ninja-tutorial/pages/ninjas/index.js
@@ -1,9 +1,27 @@
 // route => /ninjas
+import styles from "@/styles/Ninjas.module.css";
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+}; // implement before render
+
+const Ninjas = (props) => {
   return (
     <div>
       <h1>hi All Ninjas</h1>
+      {props.ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
