18c491f5874546d3ce8f0a63bbfb5da0ab0888fe
RuFrontEnd <k718198@gmail.com>
2022-06-03 19:56:13 +0800

    新增 dynamic route

---
+++ net-ninja-tutorial/pages/ninjas/[id].js
@@ -0,0 +1,9 @@
+const Details = () => {
+  return (
+    <div>
+      <h1>Deatails Page</h1>
+    </div>
+  );
+};
+
+export default Details;
--- net-ninja-tutorial/pages/ninjas/index.js
+++ net-ninja-tutorial/pages/ninjas/index.js
@@ -1,5 +1,6 @@
 // route => /ninjas
 import styles from "@/styles/Ninjas.module.css";
+import Link from "next/link";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -16,11 +17,11 @@ const Ninjas = (props) => {
     <div>
       <h1>hi All Ninjas</h1>
       {props.ninjas.map((ninja) => (
-        <div key={ninja.id}>
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
