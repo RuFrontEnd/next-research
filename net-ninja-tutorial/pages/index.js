84e30168064f45b00883957d492210795b150abe
RuFrontEnd <k718198@gmail.com>
2022-06-03 15:23:19 +0800

    新增 module css

--- net-ninja-tutorial/pages/index.js
+++ net-ninja-tutorial/pages/index.js
@@ -1,7 +1,7 @@
 // 路由首頁
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.css"; // [name].module.css => 支援 styles.className 寫法, 可避免全域汙染問題
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Link from "next/link";
@@ -9,19 +9,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quisquam
         quae nisi? Fuga eaque officia tenetur fugit assumenda et nihil animi
         laboriosam quas autem, veniam, quo consequatur unde facere consequuntur.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quisquam
         quae nisi? Fuga eaque officia tenetur fugit assumenda et nihil animi
         laboriosam quas autem, veniam, quo consequatur unde facere consequuntur.
       </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
