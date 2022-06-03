0789a1c8fce3b33077a20ba1af73e2a65ff24821
RuFrontEnd <k718198@gmail.com>
2022-06-03 19:00:14 +0800

    修改 meta data

--- net-ninja-tutorial/pages/about.js
+++ net-ninja-tutorial/pages/about.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 // sfc => Simple React Snippets
 // pages 下檔案即路由
 const About = () => {
   return (
     <div>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="About" />
+      </Head>
       <h1>About</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
--- net-ninja-tutorial/pages/index.js
+++ net-ninja-tutorial/pages/index.js
@@ -1,28 +1,33 @@
 // 路由首頁
-import Head from "next/head";
-import Image from "next/image";
+import Head from "next/head"; // revise meta data
 import styles from "../styles/Home.module.css"; // [name].module.css => 支援 styles.className 寫法, 可避免全域汙染問題
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quisquam
-        quae nisi? Fuga eaque officia tenetur fugit assumenda et nihil animi
-        laboriosam quas autem, veniam, quo consequatur unde facere consequuntur.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quisquam
-        quae nisi? Fuga eaque officia tenetur fugit assumenda et nihil animi
-        laboriosam quas autem, veniam, quo consequatur unde facere consequuntur.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjajs" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+          quisquam quae nisi? Fuga eaque officia tenetur fugit assumenda et
+          nihil animi laboriosam quas autem, veniam, quo consequatur unde facere
+          consequuntur.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+          quisquam quae nisi? Fuga eaque officia tenetur fugit assumenda et
+          nihil animi laboriosam quas autem, veniam, quo consequatur unde facere
+          consequuntur.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
