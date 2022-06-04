8ccc4082fe149900042f7bbb94acc2cf2d124cfc
RuFrontEnd <k718198@gmail.com>
2022-06-04 10:50:04 +0800

    新增 dynamic routing

--- net-ninja-tutorial/pages/ninjas/[id].js
+++ net-ninja-tutorial/pages/ninjas/[id].js
@@ -1,7 +1,31 @@
-const Details = () => {
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  const paths = data.map(ninja => ({ params: { id: ninja.id.toString() } }));
+
+  return {
+    paths,
+    fallback: true, // false or 'blocking'
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+}
+
+const Details = (props) => {
   return (
     <div>
-      <h1>Deatails Page</h1>
+      <h1>{props.ninja.nae}</h1>
+      <p>{props.ninja.email}</p>
+      <p>{props.ninja.website}</p>
+      <p>{props.ninja.address.city}</p>
     </div>
   );
 };
