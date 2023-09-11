bd7b933b77b5fc91fe60b1b5c10f5c7e07dc3816
Luke_Xie <k718198@gmail.com>
2023-09-11 21:22:51 +0800

    新增 SSR 方法

---
+++ nextjs-blog/pages/getServerSideProps.js
@@ -0,0 +1,27 @@
+// 根據 Next.js 官方說明，如果需要每次使用者瀏覽網頁時，伺服器都能呼叫 API，將最新的資料都注入到 HTML，則可以選擇使用 getServerSideProps
+// 如果需要每次使用者瀏覽網頁時，伺服器都能呼叫 API，將最新的資料都注入到 HTML，則可以選擇使用 getServerSideProps
+import Head from 'next/head';
+
+// 每一次請求都會拿到資料
+export async function getServerSideProps() {
+    // 呼叫其他api拿取資料
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await res.json()
+
+    // 傳送data到頁面的props
+    return { props: { data } }
+}
+
+
+export default function SSR({ data }) { // data props from getServerSideProps
+    return (
+        <>
+            <Head>
+                <title>
+                    fetch data
+                </title>
+            </Head>
+            {data.id}
+        </>
+    );
+}
